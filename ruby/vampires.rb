03bcd395b95763b4395aed92aa57f6528691d69d
Samantha Cooks <samtcooks@gmail.com>
2017-03-09 14:57:33 -0500

    Add vampire questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+puts "What's your name?"
+name=gets.chomp
+
+puts "How old are you?"
+age=gets.chomp
+
+puts "What year were you born?"
+birth_year=gets.chomp
+
+puts "Our cafeteria serves garlic bread. Should we order some for you? [y/n]"
+garlic_bread=gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+insurance=gets.chomp
