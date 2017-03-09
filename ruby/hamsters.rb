b16871a6d4404a56183a791e456b80bfa82599a6
Samantha Cooks <samtcooks@gmail.com>
2017-03-08 20:54:55 -0500

    Completed printing of hamster questionarre.

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -9,18 +9,27 @@ fur_color=gets.chomp
 
 puts "Is the hamster a good candidate for adoption? [y/n]"
 adoption=gets.chomp
-if adoption= "y"
+if adoption== "y"
   adoption=true
-elsif adoption="n"
+elsif adoption=="n"
   adoption=false
 else
 adoption=nil
 end
 
-puts "What is the estimated age of the hamster?"
+puts "What is the estimated age of the hamster? (years)"
 age=gets.chomp.to_i
 
 if age==""
  age=nil
 end
 
+
+
+puts "My hamster's name is #{hamster_name}. It's volume_level is #{volume_level}. It has beautiful fur the color of #{fur_color}. My hamster is #{age} years old."
+ 
+if adoption
+  puts "My hamster is a good candidate for adoption"
+else
+  puts "My hamster is not a good candidate for adoption"
+end
