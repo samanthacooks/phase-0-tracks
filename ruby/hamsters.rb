2374bda2d7614fa6318954d4af6dbd2f2a7cf315
Samantha Cooks <samtcooks@gmail.com>
2017-03-08 20:46:13 -0500

    Convert strings to integers and boolean

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -2,16 +2,23 @@ puts "What's your hamster's name?"
 hamster_name=gets.chomp
 
 puts "Volume level? On a scale of 1-10."
-volume_level=gets.chomp
+volume_level=gets.chomp.to_i
 
 puts "Fur color?"
 fur_color=gets.chomp
 
 puts "Is the hamster a good candidate for adoption? [y/n]"
 adoption=gets.chomp
+if adoption= "y"
+  adoption=true
+elsif adoption="n"
+  adoption=false
+else
+adoption=nil
+end
 
 puts "What is the estimated age of the hamster?"
-age=gets.chomp
+age=gets.chomp.to_i
 
 if age==""
  age=nil
