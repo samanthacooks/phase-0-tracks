81c5cbc56f6306ad96cc7f9ee6f5c92ed43736f3
Samantha Cooks <samtcooks@gmail.com>
2017-03-15 02:15:55 -0400

    Add numerous array and hash methods to meet required conditions

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,8 +1,21 @@
-def hollywood
- puts "hello fake"
- actor_holly = ["lee", "brad", "andy", "scarlet"]
+puts "hello fake"
+ actor_holly = [ "lee", "brad", "andy", "scarllet"]
+ actors = { "lee" => "cradle to the grave", "brad" => "mr. & mrs. Smith", "andy" => "godfatherIII" }
   
-end
 
-hollywood.each { |x| puts "#{x} TEST"}
 
+#actor_holly.each { |x| puts "#{x} TEST"}
+#actors.each { |x| puts "#{x} TEST"}
+
+#actor_holly.map! { |x| puts "#{x} film"}
+#actors.map {|x| puts "#{x} film"}
+
+#actor_holly.delete_if { |x| x.length > 4 }
+#p actor_holly
+#actors.delete_if { |x, y| y.length > 15 }
+#p actors
+#actor_holly.select! {|x| x =~ /["e"]/ } 
+#actors.select! {|x, y| y =~ /["a"]/ }
+#actor_holly.keep_if {|x| x =~ /["e"]/ }
+#actors.keep_if {|x,y| y =~ /["a"]/ }
+actor_holly.take_while {|x| x.slice(2) }
