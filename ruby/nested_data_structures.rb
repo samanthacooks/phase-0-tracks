b209a04424a1f38702d4c20f861f8e67f13abcd7
Samantha Cooks <samtcooks@gmail.com>
2017-03-16 00:52:03 -0400

    Init commit

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,32 @@
+suitcase = {
+  toiletry_bag: {
+      makeup_bag: [
+        "face moisturizer", 
+        "mascara", 
+        "brush"],
+      medicine_bag: [
+        "aleve",
+        "tea tree oil"]
+   },
+   clothing_bag: {
+      clean_clothes: [
+        "sweater",
+        "jeans",
+        "shirt"],
+      dirty_clothes: [
+      ],
+      jewelry: {
+        pair_of_earrings: 3,
+        necklaces: 2
+      }
+   },
+   shoe_bag: {
+      shoes: [
+        "sneakers",
+        "boots"],
+      socks: [
+        "ankle cut",
+        "fuzzy"]
+   }
+}
+
