fa27136f6730a8978647993cc3379acf54c6cb32
Samantha Cooks <samtcooks@gmail.com>
2017-03-16 11:49:11 -0400

    update release 1

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -9,8 +9,40 @@ date = gets.chomp.to_i
 
 march_saturdays.each_with_index {|x,y| 
   if date == x
-    puts "#{y}"
+  puts "#{y}"
   else
-    puts "nil"
+  puts "nil"
   end
 }
+
+# Release 1
+
+# Begin with array with 0, 1
+# Create fibonacci method to add additional numbers to array
+# Print array
+
+
+puts "How many sequences of Fibonacci would you like to run?"
+input = gets.chomp.to_i
+
+def fib(input)
+ fibonacci = []
+ if input == 0
+  fibonacci.push(0)
+ elsif input == 1
+  fibonacci.push(0)
+  fibonacci.push(1)
+ else
+  fibonacci.push(0)
+  fibonacci.push(1)
+  current_index = 2
+  until input == current_index
+  new_integer = (fibonacci[current_index - 1]) + (fibonacci[current_index - 2])
+  fibonacci.push(new_integer)
+  current_index += 1
+  end
+ end
+ p fibonacci
+end
+
+fib(input)
