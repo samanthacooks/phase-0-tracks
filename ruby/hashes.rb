85294bd22ce8c2c7f838da8d21f836d5a7ee266d
Samantha Cooks <samtcooks@gmail.com>
2017-03-15 20:37:33 -0400

    Complete psuedo code

---
+++ ruby/hashes.rb
@@ -0,0 +1,15 @@
+#Interior Designer Form Code
+#Ask the user to enter details of each client
+#Inputs include: client's name, age, number of children, decor them, budget, and immediate job
+#Make all keys symbols in the hash, unless string is absolutely necessary
+
+#Program should:
+  #Prompt user for all info
+  #Convert user input to the appropriate data type
+  #Print the hash back when all quesitons have been asnswered
+  #Give user the oppurunity to udate a key once. If user replies "none", skip. If the user updates a key, ask for the new value and update key
+  #Print the latest version of the hash
+  #Exit program
+
+
+
