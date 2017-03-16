199bbfc09440e62a515051aad97a2e9611a09918
Samantha Cooks <samtcooks@gmail.com>
2017-03-15 22:08:04 -0400

    Declare methods for each iteration

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,14 +1,31 @@
-puts "hello fake"
- actor_holly = [ "lee", "brad", "andy", "scarllet"]
- actors = { "lee" => "cradle to the grave", "brad" => "mr. & mrs. Smith", "andy" => "godfatherIII" }
-  
 
 
-#actor_holly.each { |x| puts "#{x} TEST"}
-#actors.each { |x| puts "#{x} TEST"}
+actor_holly = [ "lee", "brad", "andy", "scarllet"]
+actors = { "lee" => "cradle to the grave", "brad" => "mr. & mrs. Smith", "andy" => "godfatherIII" }
+    
+#iterating array using .each
+def inter_each(actor_holly)
+  actor_holly.each { |x| puts "#{x} TEST"}
+end
+p inter_each(actor_holly)
 
-#actor_holly.map! { |x| puts "#{x} film"}
-#actors.map {|x| puts "#{x} film"}
+#iterating  hash using .each
+def inter_actors(actors)
+  actors.each { |x| puts "#{x} TEST"}
+end
+p inter_actors(actors)
+
+#iterating using .map!
+def inter_bang(actor_holly)
+    actor_holly.map! { |x| puts "#{x} film"}
+end
+p inter_bang(actor_holly)
+
+#interating a hash using map , as on Hashes we don't use map! 
+def inter_nobang(actors)
+    actors.map {|x| puts "#{x} film"}
+end
+p inter_nobang(actors)
 
 #actor_holly.delete_if { |x| x.length > 4 }
 #p actor_holly
@@ -18,4 +35,5 @@ puts "hello fake"
 #actors.select! {|x, y| y =~ /["a"]/ }
 #actor_holly.keep_if {|x| x =~ /["e"]/ }
 #actors.keep_if {|x,y| y =~ /["a"]/ }
-actor_holly.take_while {|x| x.slice(2) }
+#actor_holly.index{|x| x == "lee"}
+#actors.index{|x,y| y=="e"}
