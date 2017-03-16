1701ab4346d6822404d7b0a791482d8a1e736af9
Samantha Cooks <samtcooks@gmail.com>
2017-03-16 09:36:20 -0400

    Init commit

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,6 @@
+#Alias for Spy Name
+#Write a code that creates an alias for user input's name
+#Alias must meet the following requirements: 
+# - swap the first and last name
+# - change all the vowels to the next vowel in 'aeiou'
+# - change all consonant to the next consonant in the alphabet 
