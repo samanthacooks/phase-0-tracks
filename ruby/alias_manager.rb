49655255d8640478e79481d9957c764232694329
Samantha Cooks <samtcooks@gmail.com>
2017-03-20 10:23:16 -0400

    Switch first and last name

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -4,3 +4,19 @@
 # - swap the first and last name
 # - change all the vowels to the next vowel in 'aeiou'
 # - change all consonant to the next consonant in the alphabet 
+
+
+
+
+puts "What is your first name?"
+first_name = gets.chomp
+
+puts "What is your last name?"
+last_name = gets.chomp
+
+
+
+spy_name = {first_name,last_name}
+spy_name[0],spy_name[1] = spy_name[1],spy_name[0]
+
+puts spy_name
