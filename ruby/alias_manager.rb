da6eba2352b0b50b3ea77c6c5776c704859da613
Samantha Cooks <samtcooks@gmail.com>
2017-03-20 10:21:23 -0400

    Switch first and last name in array

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,6 +1,23 @@
 #Alias for Spy Name
-#Write a code that creates an alias for user input's name
+##Write a code that creates an alias for user input's name
+#Ask user for first and last name
+#Store names in an array to be turned into the alias
 #Alias must meet the following requirements: 
 # - swap the first and last name
 # - change all the vowels to the next vowel in 'aeiou'
 # - change all consonant to the next consonant in the alphabet 
+
+
+
+puts "What is your first name?"
+first_name = gets.chomp
+
+puts "What is your last name?"
+last_name = gets.chomp
+
+
+
+spy_name = {first_name,last_name}
+spy_name[0],spy_name[1] = spy_name[1],spy_name[0]
+
+puts spy_name
