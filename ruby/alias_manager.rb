cfeb6412d3ab5c09741e7398172d0990556d1003
Samantha Cooks <samtcooks@gmail.com>
2017-03-20 13:00:53 -0400

    Complete 5.5

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -6,6 +6,11 @@
 # - change all consonant to the next consonant in the alphabet 
 
 
+puts "Would you like an alias? [y/quit]"
+perform = gets.chomp
+
+
+until perform = "quit"
 
 
 puts "What is your first name?"
@@ -15,8 +20,39 @@ puts "What is your last name?"
 last_name = gets.chomp
 
 
+spy_name = [first_name,last_name]
 
-spy_name = {first_name,last_name}
+ user_input = user_input.new
+ 
 spy_name[0],spy_name[1] = spy_name[1],spy_name[0]
 
 puts spy_name
+
+
+
+
+# def vowel_update(x)
+# vowels = ["a", "e", "i", "o", "u"]
+
+
+spy_name = spy_name.split(" ")
+
+spy_name.map! do |x|
+if spy_name.include?(vowels) do 
+  spy_name(x).(vowels) {|x| x = vowels.next} 
+end
+end
+new_spy_name = spy_name.join(" ")
+end
+
+p vowel_update(spy_name)
+
+
+
+
+def constanent_update(x)
+  alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+end
+
+
+end
