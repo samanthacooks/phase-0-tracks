2f60ccc085f23011b866917abd5b06f8707cfec4
Samantha Cooks <samtcooks@gmail.com>
2017-03-23 09:26:51 -0400

    Complete 6.4

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,14 +1,44 @@
+# module Shout
+#   def self.yell_agrily(words)
+#     words + "!!!" + " :("
+#   end
+
+# def self.yell_happily(other_words)
+#   other_words + "!" + " :)"
+# end
+
+# end
+
+# p Shout.yell_agrily("No, I don't want to")
+# p Shout.yell_happily("Yes, I do")
+
+
+
 module Shout
-  def self.yell_agrily(words)
+  def yell_agrily(words)
     words + "!!!" + " :("
   end
 
-def self.yell_happily(other_words)
-  other_words + "!" + " :)"
+  def yell_happily(other_words)
+    other_words + "!" + " :)"
+  end
 end
 
+
+class Mean_friend
+  include Shout
+  def initialize()
+  end
 end
 
-p Shout.yell_agrily("No, I don't want to")
-p Shout.yell_happily("Yes, I do")
+class Nice_friend
+  include Shout
+  def initialize()
+  end
+end
 
+mean = Mean_friend.new
+p mean.yell_agrily("Go")
+
+nice = Nice_friend.new
+p nice.yell_happily("Don't go")
