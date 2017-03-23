adfd74209a8cbc1c0013cdceb821fa95e7b2cdc2
Samantha Cooks <samtcooks@gmail.com>
2017-03-23 01:02:40 -0400

    Complete 6.3

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,8 @@
 class Santa
 
+attr_reader :age, :ethnicity
+attr_accessor :gender
+
   def initialize(gender,ethnicity)
     "Initializing Santa instance..."
     @gender = gender
@@ -16,35 +19,39 @@ class Santa
     "That was a good #{cookie}!"
   end
 
+
 #setter
-def celebrate_birthday(new_age)
+def celebrate_birthday()
   @age.to_i + 1
 end
 
-def get_mad_at()
-
-
-#getter
-def age
-  @age
+def get_mad_at(name)
+  @reindeer_ranking.delete(name)
+  @reindeer_ranking.push(name)
 end
 
-def ethinicity
-  @ethnicity
+def new_age
+  @age = rand(1..140)
 end
-
 
 end
 
-# new_york = Santa.new
 
-# p new_york.speak
-# p new_york.eat_milk_and_cookies("gingerbread")
+new_york = Santa.new("female","west indian")
+p new_york.speak
+p new_york.eat_milk_and_cookies("gingerbread")
+
+
+austin = Santa.new("trans female", "mexican")
+p "Age: #{austin.age} and Ethnicity: #{austin.ethnicity} and Gender: #{austin.gender}"
+austin.celebrate_birthday
+austin.gender = "female"
+austin.get_mad_at("Rudolph")
+p "Age: #{austin.celebrate_birthday} and Ethnicity: #{austin.ethnicity} and Gender: #{austin.gender}"
 
 
 
 santas = []
-
 
 early_genders = ["trans male", "gender non conforming", "female", "two-spirit", "other"]
 early_ethinicities = ["black", "hispanic", "other", "korean", "eritrean"]
@@ -52,5 +59,19 @@ early_ethinicities = ["black", "hispanic", "other", "korean", "eritrean"]
 early_ethinicities.length.times do |i|
   santas << Santa.new(early_genders[i],early_ethinicities[i])
 end
+
+
+
+
+
+20.times do 
+  santas << Santa.new(early_genders.sample,early_ethinicities.sample)
+end
+
+  santas.each do |random_age|
+   random_age.new_age
+  end
+
+
 
 p santas
