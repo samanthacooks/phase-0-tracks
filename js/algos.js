d8968c14984401fc135f2107653d975f036c2b1d
Samantha Cooks <samtcooks@gmail.com>
2017-04-03 11:32:32 -0400

    Create psuedocode for function comparing 2 objects

--- js/algos.js
+++ js/algos.js
@@ -4,7 +4,7 @@
 // STEPS:
 //  1. Create a function that takes the array as the argument
 //  2. Make a loop that finds string value of each word or phrase and adds it to each parameter in a hash using .length
-//  3. Use math.max to grab the value pair that has the highest value and returns the property
+//  3. Use math.max to return the property with the highest value
 // OUTPUT: word or phrase that has the longest length
 
 
@@ -15,8 +15,25 @@ function longestInput(array){
   for (var i = 0; i < array.length; i++){
     var properties = array[i];
     object[properties] = array[i].length;
-  }
-  return object;
-}
+    var values = Object.values(object);
+    var max = Math.max.apply(null,values);}
+    // return object
+      if (object[properties] = max){
+        return properties;
+      }
+      else{
+        return console.log("Error");
+      }
+}  
 
 console.log(longestInput(array))
+
+
+// RELEASE 2
+// Create a function that compares two objects checking for one shared key-value pair
+// INPUT: two objects with the same amount of key-value pairs
+// STEPS:
+//  1. Create a function separates the keys and values of each object
+//  2. Iterate over both the key array and value array for marched values 
+//  3. Use math.max to return the property with the highest value
+// OUTPUT: true or false
