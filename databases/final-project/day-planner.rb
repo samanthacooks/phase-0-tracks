869cbba7f6b0506473ced7fb0801774ef6edbfef
Samantha Cooks <samtcooks@gmail.com>
2017-04-06 15:21:34 -0400

    Create tables for days of week, tasks, and specified day tasks as well as user interface intro.

--- databases/final-project/day-planner.rb
+++ databases/final-project/day-planner.rb
@@ -12,6 +12,40 @@ require 'day-planner'
 db = SQLite3::Database.new("day-planner.db")
 
 
-# Create the table with string delimiter
-create_table_cmd = <<-SQLite3
-  CREATE TABLE IF NOT EXISTS 
+# Create the tables with string delimiter
+create_day_table = <<-SQL
+  CREATE TABLE IF NOT EXISTS days_of_week(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    weekday BOOLEAN
+  )
+  SQL
+
+  create_task_table = <<-SQL
+  CREATE TABLE IF NOT EXISTS tasks(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    minutes INT
+  )
+  SQL
+
+  create_day_task_table = <<-SQL
+  CREATE TABLE IF NOT EXISTS day_tasks(
+    id INTEGER PRIMARY KEY,
+    easy_day BOOLEAN,
+    day_id INT,
+    task_id INT,
+    FOREIGN KEY (day_id) REFERENCES days_of_week(id),
+    FOREIGN KEY (task_id) REFERENCES tasks(id)
+  )
+  SQL
+
+
+
+  # Create days of week table
+  db.execute(create_day_table)
+
+  def create_days(db, name, weekday) 
+    db.execute("INSERT INTO days_of_week (name, weekday) VALUES (?, ?)", [name, weekday])
+    db.execute("SELECT * FROM days_of_week")
+  end
