b3ed56fc5d8e8da395562ef0c4d0a9226e1f0125
Samantha Cooks <samtcooks@gmail.com>
2017-04-06 12:45:58 -0400

    Write program introductory and begin psuedocoding

--- databases/final-project/day-planner.rb
+++ databases/final-project/day-planner.rb
@@ -2,3 +2,8 @@
 
 # Create a program for the user to plan the day's or multiple day's activities. Day is based on a 24 hour period
 # and has required variables such as sleep and relax time. 
+
+
+# Require all gems
+require 'day-planner'
+
