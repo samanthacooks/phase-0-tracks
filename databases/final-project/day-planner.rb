c75da9cdb9bee4b54eac0782d54251d0054d4037
Samantha Cooks <samtcooks@gmail.com>
2017-04-06 12:43:05 -0400

    Init commit

---
+++ databases/final-project/day-planner.rb
@@ -0,0 +1,4 @@
+# DAY/WEEK PLANNER PROGRAM
+
+# Create a program for the user to plan the day's or multiple day's activities. Day is based on a 24 hour period
+# and has required variables such as sleep and relax time. 
