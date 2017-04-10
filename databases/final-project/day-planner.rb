1ec172151ae39d9946f7164b55fa8ae889f526b3
Samantha Cooks <samtcooks@gmail.com>
2017-04-09 20:15:27 -0400

    Add methods to collect each user input day to be added, as well as methods that automatically add ttasks to be planned each day

--- databases/final-project/day-planner.rb
+++ databases/final-project/day-planner.rb
@@ -24,14 +24,13 @@ create_day_table = <<-SQL
   CREATE TABLE IF NOT EXISTS tasks(
     id INTEGER PRIMARY KEY,
     name VARCHAR(255),
-    minutes INT
+    hours INT
   )
   SQL
 
   create_day_task_table = <<-SQL
   CREATE TABLE IF NOT EXISTS day_tasks(
     id INTEGER PRIMARY KEY,
-    easy_day BOOLEAN,
     day_id INT,
     task_id INT,
     FOREIGN KEY (day_id) REFERENCES days_of_week(id),
@@ -39,52 +38,108 @@ create_day_table = <<-SQL
   )
   SQL
 
-
   # Create days of week table
-  db.execute(create_day_table)
-
-  def create_days(db, name, weekday) 
-    db.execute("INSERT INTO days_of_week (name, weekday) VALUES (?, ?)", [name, weekday])
-    db.execute("DELETE FROM days_of_week WHERE name='none'")
-    db.execute("SELECT name FROM days_of_week")
-  end
+db.execute(create_day_table)
+db.execute(create_task_table)
+db.execute(create_day_task_table)
 
   def clear_days_of_week_table(db)
-  db.execute("DELETE FROM days_of_week")
+    db.execute("DELETE FROM days_of_week")
+  end
+
+  def clear_task_table(db)
+    db.execute("DELETE FROM tasks")
+  end
+
+  def clear_day_task_table(db)
+    db.execute("DELETE FROM day_tasks")
   end
 
 
+  def standard_week(db)
+      real_week_days = {"monday" => "true", "tuesday" => "true", "wednesday" => "true", "thursday" => "true", "friday" => "true", "saturday" => "true", "sunday" => "true"}
+      real_week_days.each do |days, weekday|
+        db.execute("INSERT INTO days_of_week (name,weekday) VALUES (?,?)",[days,weekday])
+      end
+    db.execute("SELECT id,name FROM days_of_week")
+  end
 
+  def create_days(db, day_id) 
+    db.execute("INSERT INTO day_tasks (day_id) VALUES (?)", [day_id])
+    db.execute("DELETE FROM day_tasks WHERE day_id='none'")
+    db.execute("SELECT day_tasks.id, days_of_week.name FROM day_tasks, days_of_week WHERE day_tasks.day_id = days_of_week.id")
+  end
 
+  def other_tasks(db)
+  other_tasks = {"tv hour" => 1, "exercise" => 1, "recreation activity" => 2, "call family" => 1}
+    other_tasks.each do |tasks, hours|
+      db.execute("INSERT INTO tasks (name,hours) VALUES (?,?)",[tasks,hours])
+    end
+  db.execute("SELECT * FROM tasks")
+  end
 
+  def weekday_tasks(db)
+  weekday_tasks = {"grooming" => 1, "breakfast" => 0.5, "lunch" => 1, "dinner" => 1, "relax time" => 1, "sleep" => 6, "DBC class" => 8.5, "study" => 3}
+      weekday_tasks.each do |tasks, hours|
+      db.execute("INSERT INTO tasks (name,hours) VALUES (?,?)",[tasks,hours])
+    end
+    p db.execute("SELECT * FROM tasks")
+  end
+
+  def weekend_tasks(db)
+  weekend_tasks = {"grooming" => 1, "breakfast" => 0.5, "lunch" => 1, "dinner" => 1, "relax time" => 1, "sleep" => 6, "study" => 5}
+      weekend_tasks.each do |tasks, hours|
+      db.execute("INSERT INTO tasks (name,hours) VALUES (?,?)",[tasks,hours])
+    end
+    p db.execute("SELECT * FROM tasks")
+  end
+
+  def print_tasks(db)
+    db.execute("SELECT * FROM day_tasks")
+    # db.execute("SELECT day_tasks.id, days_of_week.name, tasks.name, tasks.hours FROM day_tasks, days_of_week, tasks WHERE day_tasks.day_id = days_of_week.id AND day_tasks.task_id = tasks.id")
+  end
 
 
 
   #Introduce program to user and start asking for user input
 
 p "Welcome to the Sammie CBCB day planner program. Here you will be able to plan your day or week by task time durations while you are a student at DBC."
+clear_day_task_table(db)
 
+day_id = ""
+until day_id == "none"
+clear_days_of_week_table(db)  
+  p "Enter number for what day you would like to plan for from the table below. If no more, enter 'none'."
+  p standard_week(db)
+  day_id = gets.chomp
 
-p "What day of the week will you like to plan for? If no more enter 'none'."
-plan_day = gets.chomp
-
-until plan_day == "none"
-    if plan_day = "monday" || "tuesday" || "wednesday" || "thursday" || "friday"
-      weekday = "true"
-      p "Since today is a weekday, DBC class has automatically been added to your day. Enter another day or 'none' if done."
-    else
-      weekday = "false"
-      p "It's the weekend! No DBC class today, but study time has been added automatically to your day. Enter another day or 'none' if done."
-    end
-  plan_day = gets.chomp
-  create_days(db,plan_day,weekday)
+  create_days(db,day_id)
 end
 
-p "See below for days set to be planned."
-p create_days(db,plan_day,weekday)
+p "Below are the days you are planning for this session."
+p create_days(db,day_id)
 
-clear_days_of_week_table(db)
+to_be_planned = create_days(db,day_id)
 
-# Won't save the first entry for the first day to be planned.
+real_week_days = ["monday", "tuesday", "wednesday", "thursday", "friday"]
+real_weekend_days = ["saturday", "sunday"]
+
+clear_task_table(db)
+to_be_planned.each do |key,value|
+  if real_week_days.include? value
+    weekday_tasks(db)
+  else 
+    real_weekend_days.include? value
+    weekday_tasks(db)
+  end
+end
+
+# p weekday_tasks(db)
+# p weekend_tasks(db)
+
+p print_tasks(db)
+
+
+#can't get each tasks to apply to each day individually. will print separately but is still nill in day_task table
 
 
