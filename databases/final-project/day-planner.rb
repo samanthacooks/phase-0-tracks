cdf267817313b980496f2f87c0493ddd43b1db60
Samantha Cooks <samtcooks@gmail.com>
2017-04-10 04:21:01 -0400

    Added methods to update standard weekday and weekend hashes.

--- databases/final-project/day-planner.rb
+++ databases/final-project/day-planner.rb
@@ -28,13 +28,41 @@ create_day_table = <<-SQL
   )
   SQL
 
+  create_weekday_task_table = <<-SQL
+  CREATE TABLE IF NOT EXISTS weekday_tasks(
+    id INTEGER PRIMARY KEY,
+    day_id INT,
+    name VARCHAR(255),
+    hours INT,
+    other_task_id INT,
+    FOREIGN KEY (day_id) REFERENCES days_of_week(id),
+    FOREIGN KEY (other_task_id) REFERENCES other_tasks(id)
+  )
+  SQL
+
+  create_weekend_task_table = <<-SQL
+  CREATE TABLE IF NOT EXISTS weekend_tasks(
+    id INTEGER PRIMARY KEY,
+    day_id INT,
+    name VARCHAR(255),
+    hours INT,
+    other_task_id INT,
+    FOREIGN KEY (day_id) REFERENCES days_of_week(id),
+    FOREIGN KEY (other_task_id) REFERENCES other_tasks(id)
+  )
+  SQL
+
   create_day_task_table = <<-SQL
   CREATE TABLE IF NOT EXISTS day_tasks(
     id INTEGER PRIMARY KEY,
     day_id INT,
     task_id INT,
+    weekday_task_id INT,
+    weekend_task_id INT,
     FOREIGN KEY (day_id) REFERENCES days_of_week(id),
-    FOREIGN KEY (task_id) REFERENCES tasks(id)
+    FOREIGN KEY (task_id) REFERENCES tasks(id),
+    FOREIGN KEY (weekday_task_id) REFERENCES weekday_tasks(id),
+    FOREIGN KEY (weekend_task_id) REFERENCES weekend_tasks(id)
   )
   SQL
 
@@ -42,6 +70,8 @@ create_day_table = <<-SQL
 db.execute(create_day_table)
 db.execute(create_task_table)
 db.execute(create_day_task_table)
+db.execute(create_weekday_task_table)
+db.execute(create_weekend_task_table)
 
   def clear_days_of_week_table(db)
     db.execute("DELETE FROM days_of_week")
@@ -55,6 +85,13 @@ db.execute(create_day_task_table)
     db.execute("DELETE FROM day_tasks")
   end
 
+  def clear_weekday_task_table(db)
+    db.execute("DELETE FROM weekday_tasks")
+  end
+
+  def clear_weekend_task_table(db)
+    db.execute("DELETE FROM weekend_tasks")
+  end
 
   def standard_week(db)
       real_week_days = {"monday" => "true", "tuesday" => "true", "wednesday" => "true", "thursday" => "true", "friday" => "true", "saturday" => "true", "sunday" => "true"}
@@ -81,17 +118,17 @@ db.execute(create_day_task_table)
   def weekday_tasks(db)
   weekday_tasks = {"grooming" => 1, "breakfast" => 0.5, "lunch" => 1, "dinner" => 1, "relax time" => 1, "sleep" => 6, "DBC class" => 8.5, "study" => 3}
       weekday_tasks.each do |tasks, hours|
-      db.execute("INSERT INTO tasks (name,hours) VALUES (?,?)",[tasks,hours])
+      db.execute("INSERT INTO weekday_tasks (name,hours) VALUES (?,?)",[tasks,hours])
     end
-    p db.execute("SELECT * FROM tasks")
+    db.execute("SELECT * FROM weekday_tasks")
   end
 
   def weekend_tasks(db)
   weekend_tasks = {"grooming" => 1, "breakfast" => 0.5, "lunch" => 1, "dinner" => 1, "relax time" => 1, "sleep" => 6, "study" => 5}
       weekend_tasks.each do |tasks, hours|
-      db.execute("INSERT INTO tasks (name,hours) VALUES (?,?)",[tasks,hours])
+      db.execute("INSERT INTO weekend_tasks (name,hours) VALUES (?,?)",[tasks,hours])
     end
-    p db.execute("SELECT * FROM tasks")
+    db.execute("SELECT * FROM weekend_tasks")
   end
 
   def print_tasks(db)
@@ -99,7 +136,20 @@ db.execute(create_day_task_table)
     # db.execute("SELECT day_tasks.id, days_of_week.name, tasks.name, tasks.hours FROM day_tasks, days_of_week, tasks WHERE day_tasks.day_id = days_of_week.id AND day_tasks.task_id = tasks.id")
   end
 
+  def print_(db)
+    db.execute("SELECT * FROM day_tasks")
+    # db.execute("SELECT day_tasks.id, days_of_week.name, tasks.name, tasks.hours FROM day_tasks, days_of_week, tasks WHERE day_tasks.day_id = days_of_week.id AND day_tasks.task_id = tasks.id")
+  end
 
+  def add_other_task_to_weekday(db,other_task_id,day)
+    db.execute("INSERT INTO weekday_tasks (other_task_id) VALUES (?)",[other_task_id])
+    db.execute("SELECT * FROM weekday_tasks")
+  end
+
+  def add_other_task_to_weekend(db,other_task_id)
+    db.execute("INSERT INTO weekend_tasks (other_task_id) VALUES (?)",[other_task_id])
+    db.execute("SELECT * FROM weekend_tasks")
+  end
 
   #Introduce program to user and start asking for user input
 
@@ -107,12 +157,11 @@ p "Welcome to the Sammie CBCB day planner program. Here you will be able to plan
 clear_day_task_table(db)
 
 day_id = ""
-until day_id == "none"
-clear_days_of_week_table(db)  
+until day_id == "none" 
+clear_days_of_week_table(db) 
   p "Enter number for what day you would like to plan for from the table below. If no more, enter 'none'."
   p standard_week(db)
   day_id = gets.chomp
-
   create_days(db,day_id)
 end
 
@@ -124,22 +173,49 @@ to_be_planned = create_days(db,day_id)
 real_week_days = ["monday", "tuesday", "wednesday", "thursday", "friday"]
 real_weekend_days = ["saturday", "sunday"]
 
-clear_task_table(db)
+p "We pre-populated standard tasks of your day depending on whether it is a weekday or the weekend. See below! Don't worry, you can update if you need to later."
+clear_weekday_task_table(db)
+clear_weekend_task_table(db)
 to_be_planned.each do |key,value|
   if real_week_days.include? value
-    weekday_tasks(db)
+    p value
+    p weekday_tasks(db)
   else 
     real_weekend_days.include? value
-    weekday_tasks(db)
+    p value
+    p weekend_tasks(db)
   end
 end
 
-# p weekday_tasks(db)
-# p weekend_tasks(db)
+day_to_be_updated = ""
+until day_to_be_updated = "no"
+p "Do you need to add a task to one of these days? If yes, enter which day. If not, enter 'no'."
+day_to_be_updated = gets.chomp
 
-p print_tasks(db)
+if to_be_planned.has_value?(day_to_be_updated) 
+  task_to_add = ""
+  until task_to_add = "done"
+  p "See below for table of other tasks to be added. Select number of task you would like to add to today. If no more, enter 'done'."
+  p other_tasks(db)
+  task_to_add = gets.chomp
+    if real_week_days.include? day_to_be_updated
+      p add_other_task_to_weekday(db,task_to_add)
+    else real_weekend_days.include? day_to_be_updated
+      p add_other_task_to_weekend(db,task_to_add)
+    end
+  end
+else
+end
+
+p "Please be warned that the below days have a task duration of over 24 hours for the day. I know, we all wish we could have more than 24 hours in a day."
 
 
-#can't get each tasks to apply to each day individually. will print separately but is still nill in day_task table
+
+
+# need to add if statements to add other tasks to the specified set tasks for the weekend and weekday. 
+# need to add a method to delete a tasks from the set tasks for weekend and weekday
+# develop a method that sums up the values of the hours in the task hash, then warns user about being over 24 hours
+# print final version of days with planned tasks (maybe print in a sentence too)
+# finalize code
 
 
