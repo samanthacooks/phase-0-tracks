15d2e6e9530ae987f6dc7d122e79fca20b43e802
Samantha Cooks <samtcooks@gmail.com>
2017-04-12 19:35:30 -0400

    Complete jquery project, demonstrating basic understanding

--- web_dev/jquery/jquery_project/frisco.js
+++ web_dev/jquery/jquery_project/frisco.js
@@ -1,16 +1,17 @@
 $(document).ready(function(){
 
+
 $(".picture:eq(1)").hide(500).show(800);
-
-
-$function changeButtontext(){
-button.onclick
-}
-
 
 $(".picture:not(2)").animate({opacity: 0.7}, 500);
 
+$(".med-header").hover(function(){
+  $(this).css("color", "deeppink")
+});
 
+$("button").click(function(){
+  alert("You clicked it :)");
+});
 
 
   });
