0db2ddea5d63800cabfe92099c09b4dbe5134334
Samantha Cooks <samtcooks@gmail.com>
2017-04-12 17:51:21 -0400

    Start jquery code on finished html file

--- web_dev/jquery/jquery_project/frisco.js
+++ web_dev/jquery/jquery_project/frisco.js
@@ -1,8 +1,16 @@
 $(document).ready(function(){
 
+$(".picture:eq(1)").hide(500).show(800);
+
+
+$function changeButtontext(){
+button.onclick
+}
+
+
+$(".picture:not(2)").animate({opacity: 0.7}, 500);
 
 
 
 
-
-  )};
+  });
